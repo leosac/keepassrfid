6fc41caee45a1bdcc4e2dac2107b12744509db67
Maxhy <maxime.chamley@leosac.com>
2022-10-20 09:04:33 +0200

    Update to LLA 2.4.2

--- keepassrfid/Properties/AssemblyInfo.cs
+++ keepassrfid/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("KeePassRFID")]
 [assembly: AssemblyDescription("KeePass RFID/NFC Plugin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Leosac")]
+[assembly: AssemblyCompany("Leosac SAS")]
 [assembly: AssemblyProduct("KeePass Plugin")]
 [assembly: AssemblyCopyright("Copyright © Leosac 2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Leosac")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
